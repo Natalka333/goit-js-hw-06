d37514e26c6483b517e9b61a7083a1b1c80be9fb
Natalka333 <126109559+Natalka333@users.noreply.github.com>
2023-06-29 15:19:11 +0300

    Update task-01.js

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,7 +1,11 @@
-const categoriesEl = document.querySelectorAll('.item');
-console.log(categoriesEl);
-const itemEl = document.querySelectorAll('.item > h2');
-console.log(itemEl);
-itemEl.forEach(item => {
-  console.log(item);
+const categoriesAll = document.querySelectorAll('.item').length;
+console.log(`Number of categories: ${categoriesAll}`);
+
+const categoriesList = document.querySelectorAll('.item');
+
+categoriesList.forEach(category => {
+  const categoryName = category.querySelector('h2').textContent;
+  const categoryElements = category.querySelectorAll('li').length;
+  console.log(`Category: ${categoryName}`);
+  console.log(`Elements: ${categoryElements}`);
 });
