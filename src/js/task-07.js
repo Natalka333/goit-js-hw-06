5b44d9d5bc9969e3045e793c9059d4b01125c9b2
Natalka333 <126109559+Natalka333@users.noreply.github.com>
2023-07-01 18:35:17 +0300

    Update task-07.js

---
+++ src/js/task-07.js
@@ -0,0 +1,5 @@
+const inputEl = document.querySelector('#font-size-control');
+const spanEl = document.querySelector('#text');
+spanEl.style.fonSize = '16';
+
+inputEl.addEventListener('input', event => {});
