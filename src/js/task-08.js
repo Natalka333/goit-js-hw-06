9f45e1f64541a5c18d0b9851e2e91640a7342dd6
Natalka333 <126109559+Natalka333@users.noreply.github.com>
2023-07-01 18:35:11 +0300

    Update task-08.js

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,13 @@
+const formEl = document.querySelector('.login-form');
+formEl.addEventListener('submit', onSubmit);
 
+function onSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === '' || password.value === '') {
+    alert('Please fill in all fields !');
+  }
+}
