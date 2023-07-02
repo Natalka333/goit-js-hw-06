563c2e53fe2e20ca23c3a9c9effd2ad58effdda7
Natalka333 <126109559+Natalka333@users.noreply.github.com>
2023-07-02 16:44:34 +0300

    Update task-01.js

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,11 +1,19 @@
-// const categoriesAll = document.querySelectorAll('.item').length;
-// console.log(`Number of categories: ${categoriesAll}`);
+const categoriesAll = document.querySelectorAll('.item').length;
+console.log(`Number of categories: ${categoriesAll}`);
 
-// const categoriesList = document.querySelectorAll('.item');
+const categoriesList = document.querySelectorAll('.item');
 
 // categoriesList.forEach(category => {
 //   const categoryName = category.querySelector('h2').textContent;
 //   const categoryElements = category.querySelectorAll('li').length;
+//   console.log(`Category: ${categoryName}`);
+//   console.log(`Elements: ${categoryElements}`);
+// });
+
+// 2 вариант
+// categoriesList.forEach(category => {
+//   const categoryName = category.firstElementChild.textContent;
+//   const categoryElements = category.lastElementChild.children.length;
 //   console.log(`Category: ${categoryName}`);
 //   console.log(`Elements: ${categoryElements}`);
 // });
