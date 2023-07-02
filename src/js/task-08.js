e9b3405bedb86889e0d654bb8209e2626fb83e32
Natalka333 <126109559+Natalka333@users.noreply.github.com>
2023-07-02 16:44:16 +0300

    Update task-08.js

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,13 +1,34 @@
 const formEl = document.querySelector('.login-form');
 formEl.addEventListener('submit', onSubmit);
 
-function onSubmit(event) {
-  event.preventDefault();
-  const {
-    elements: { email, password },
-  } = event.currentTarget;
+// function onSubmit(event) {
+//   event.preventDefault();
+//   const {
+//     elements: { email, password },
+//   } = event.currentTarget;
 
-  if (email.value === '' || password.value === '') {
-    alert('Please fill in all fields !');
-  }
-}
+//   if (email.value === '' || password.value === '') {
+//     alert('Please fill in all fields !');
+//   }
+//   console.log(`Email:${email.value}, Password:${password.value}`);
+//   event.currentTarget.reset();
+// }
+
+// вариант2(
+// function onSubmit(event) {
+//   event.preventDefault();
+//   const emailInput = formEl.elements.email;
+//   const passwordInput = formEl.elements.password;
+
+//   if (emailInput.value === '' || passwordInput.value === '') {
+//     alert('Please fill in all fields !');
+//   } else {
+//     const formElData = {
+//       email: emailInput.value,
+//       password: passwordInput.value,
+//     };
+
+//     console.log(formElData);
+//     formEl.reset();
+//   }
+// }
