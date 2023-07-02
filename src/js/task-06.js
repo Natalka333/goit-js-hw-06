ef8db753e6372951bfcc09e737d782259ed56d2d
Natalka333 <126109559+Natalka333@users.noreply.github.com>
2023-07-02 16:44:23 +0300

    Update task-06.js

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -1,15 +1,30 @@
 const textEl = document.querySelector('#validation-input');
-console.log(textEl);
+// console.log(textEl);
 const inputLeng = Number(textEl.dataset.length);
-console.log(inputLeng);
+// console.log(inputLeng);
 
 textEl.addEventListener('blur', validationInput);
 
-function validationInput(value) {
-  const updateText = textEl.value.trim();
-  if (updateText.length === inputLeng) {
-    textEl.classList.add('valid');
-  } else {
-    textEl.classList.add('invalid');
-  }
-}
+// function validationInput(value) {
+//   const updateText = textEl.value.trim();
+//   if (updateText.length === inputLeng) {
+//     textEl.classList.remove('invalid');
+//     textEl.classList.add('valid');
+//   } else {
+//     textEl.classList.remove('valid');
+//     textEl.classList.add('invalid');
+//   }
+// }
+// прибирає пробіли на початку і в кінці trim();
+// 2 спосіб
+
+// function validationInput() {
+//   textEl.value.trim().length === inputLeng
+//     ? changeClass('valid', 'invalid')
+//     : changeClass('invalid', 'valid');
+// }
+
+// function changeClass(first, second) {
+//   textEl.classList.add(first);
+//   textEl.classList.remove(second);
+// }
